019514d1fb15316d62f7d516ac446d7b4be9eb5c
VoidDaoo <113223604+VoidDaoo@users.noreply.github.com>
2022-09-10 10:41:48 +0700

    Update Header.lua

--- Header.lua
+++ Header.lua
@@ -1,20 +1,20 @@
 bot_raw = {}
 ---======================---
-bot_raw["NAMEBOT1"] = {
+bot_raw["ZYZZlaid4"] = {
 webhooks = {
-  ["LINK"]  = '',
+  ["LINK"]  = 'https://discord.com/api/webhooks/1015993556449628160/KCYAG4vmiCs9x9d2VcWt0Nu2-2A5GV-7amZAWajVl9JoxNB9D6vurjyr8DdaATaf1CGx',
   ["EDIT"]  = false
 },
 -----------------------
 bot_config = {
-  ["World"] = {"FARM1"},
-  ["Door"] = 'FARM_ID',
+  ["World"] = {"NXKSLA", "PDHXH", "WORHV", "GBIPB", "EEXTM", "MCMFQ", "TCXPG", "FQOCC", "QWHSW"},
+  ["Door"] = 'link1',
   ["Loop"] = true,
   ["Save_Mode"] = 1,
   ["Save"] = {
               {
-                ["World"] = "World_Save",
-                ["Door"] = "Door_ID",       --Mode 1
+                ["World"] = "zyzzstorages",
+                ["Door"] = "link1",       --Mode 1
                 ["Path_Seed"] = 20,
                 ["Path_Pack"] = 12
               },
@@ -31,7 +31,7 @@ bot_config = {
 },
 ------------------------
 bot_mode = {
-  ["Block"] = 5666,
+  ["Block"] = 4584,
   ["Type"] = 'fg',
   ["Ignore Gems"] = false, --Ignore Gems When Harvesting
   ["Coordinate_Break"] = {2,1}
@@ -40,21 +40,21 @@ bot_mode = {
 bot_environment = {
   ["Trash"] = {
               ["Limit"] = 120,
-              ["List"] = {5040,5042,5044,5032,5034,5036,5038,5024,5026,5028,5030,7164,7162}
+              ["List"] = {5040,5042,5044,5032,5034,5036,5038,5024,5026,5028,5030,7164,7162,1058,1098,1096,1094}
               },
   ["Buy"] = {
             ["Mode"] = true,
-            ["Name"] = 'world_lock',
-            ["Price"] = 2000,
-            ["Item_List"] = {242},
-            ["Limit"] = 3,
+            ["Name"] = 'world_lock_10_pack',
+            ["Price"] = 20000,
+            ["Item_List"] = {242,1828},
+            ["Limit"] = 10,
             ["When"] = {
                         ["Harvest"] = true,
                         ["Plant"]   = true,
                         ["Break"]   = true
                        }
             },
-  ["Jammer"] = true,
+  ["Jammer"] = false,
   ["Ban"] = {
             ["Mode"] = true,
             ["Ignore"] = {"Ignored_Name"}
